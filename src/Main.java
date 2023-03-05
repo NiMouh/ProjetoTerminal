1f4002963b46c1b38780b0058b9c6c216b026b4f
NiMouh <simaoaugusto11@hotmail.com>
2023-03-05 14:47:23 +0000

    update nos menus

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import myinputs.Ler;
 import org.deidentifier.arx.AttributeType;
 import org.deidentifier.arx.Data;
+import org.deidentifier.arx.DataHandle;
 import org.deidentifier.arx.DataType;
-import org.dom4j.datatype.DatatypeElementFactory;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class Main {
 
-    public void menuPrincipal() {
+    public static void menuPrincipal(Data dados) {
+
+        int numColumns = dados.getHandle().getNumColumns();
+        String[] headers = new String[numColumns];
+        for (int i = 0; i < numColumns; i++)
+            headers[i] = dados.getHandle().getAttributeName(i);
+
         System.out.println("\t# Bem-vindo ao programa de anonimização de dados #\n\nO que pretende fazer?\n");
         System.out.println("1 - Modificar meta-dados dos atributos");
         System.out.println("2 - Criar hierarquias");
@@ -22,39 +29,45 @@ public class Main {
         }
 
         switch (opcao) {
-            // case 1 -> menuAlterarMetadados();
-            // case 2 -> menuCriarHierarquias();
-            // case 3 -> menuAnonimizarDados();
-            // case 4 -> menuExportarDados();
-            // case 5 -> System.exit(0);
+            case 1 -> {
+                System.out.println("Qual o atributo que pretende modificar?");
+                for (int i = 0; i < numColumns; i++)
+                    System.out.println(i + 1 + " - " + headers[i]);
+                System.out.println("0 - Voltar ao menu principal");
+                int escolha = Ler.umInt();
+                while (escolha < 0 || escolha > numColumns) {
+                    System.out.println("Opção inválida. Tente novamente.");
+                    escolha = Ler.umInt();
+                }
+                if (escolha != 0)
+                    menuModificarMetaDados(dados, escolha);
+                else
+                    menuPrincipal(dados);
+            }
+            case 2 -> {
+                System.out.println("Qual o atributo que pretende criar uma hierarquia?");
+                for (int i = 0; i < numColumns; i++)
+                    System.out.println(i + 1 + "- " + headers[i]);
+                System.out.println("0 - Voltar ao menu principal");
+                int escolha = Ler.umInt();
+                while (escolha < 0 || escolha > numColumns) {
+                    System.out.println("Opção inválida. Tente novamente.");
+                    escolha = Ler.umInt();
+                }
+                if (escolha != 0)
+                    menuCriarHierarquias(dados, escolha);
+                else
+                    menuPrincipal(dados);
+            }
+            case 3 -> menuAnonimizarDados();
+            case 4 -> menuExportarDados(dados);
+            case 5 -> System.exit(0);
         }
     }
 
-
-    public static void main(String[] args) throws Exception {
-
-        String ficheiro = "data.csv";
-        Data data = Data.create(ficheiro, Charset.defaultCharset(), ';');
-
-        int numColumns = data.getHandle().getNumColumns();
-        String[] headers = new String[numColumns];
-
-        System.out.println("\t# Bem-vindo ao programa de anonimização de dados #\n\nQual item pretende selecionar?\n");
-
-
+    public static void menuModificarMetaDados(Data dados, int escolha) {
         // Variável
-        for (int i = 0; i < numColumns; i++) {
-            System.out.println(i + "- " + data.getHandle().getAttributeName(i));
-            headers[i] = data.getHandle().getAttributeName(i);
-        }
-        int escolha = Ler.umInt();
-        while (escolha < 0 || escolha > numColumns) {
-            System.out.println("Opção inválida. Tente novamente.");
-            escolha = Ler.umInt();
-        }
-
-        // Data Type
-        System.out.println("Qual o tipo de dados que esta coluna contém?\n");
+        System.out.println("Qual o tipo de dados que esta variável contém?\n");
         System.out.println("1- String");
         System.out.println("2- Integer");
         System.out.println("3- Date/Time");
@@ -66,15 +79,15 @@ public class Main {
             tipo = Ler.umInt();
         }
         switch (tipo) {
-            case 1 -> data.getHandle().getDefinition().setDataType(headers[escolha], DataType.STRING);
-            case 2 -> data.getHandle().getDefinition().setDataType(headers[escolha], DataType.INTEGER);
-            case 3 -> data.getHandle().getDefinition().setDataType(headers[escolha], DataType.DATE);
-            case 4 -> data.getHandle().getDefinition().setDataType(headers[escolha], DataType.DECIMAL);
-            case 5 -> data.getHandle().getDefinition().setDataType(headers[escolha], DataType.ORDERED_STRING);
+            case 1 -> dados.getHandle().getDefinition().setDataType(dados.getHandle().getAttributeName(escolha), DataType.STRING);
+            case 2 -> dados.getHandle().getDefinition().setDataType(dados.getHandle().getAttributeName(escolha), DataType.INTEGER);
+            case 3 -> dados.getHandle().getDefinition().setDataType(dados.getHandle().getAttributeName(escolha), DataType.DATE);
+            case 4 -> dados.getHandle().getDefinition().setDataType(dados.getHandle().getAttributeName(escolha), DataType.DECIMAL);
+            case 5 -> dados.getHandle().getDefinition().setDataType(dados.getHandle().getAttributeName(escolha), DataType.ORDERED_STRING);
         }
 
         // Identificador
-        System.out.println("Qual o tipo de identificador que esta coluna contém?\n");
+        System.out.println("Qual o tipo de identificador que esta variável contém?\n");
         System.out.println("1- Identificador");
         System.out.println("2- Identificador sensível");
         System.out.println("3- Identificador não sensível");
@@ -85,14 +98,33 @@ public class Main {
             identificador = Ler.umInt();
         }
         switch (identificador) {
-            case 1 -> data.getHandle().getDefinition().setAttributeType(headers[escolha], AttributeType.IDENTIFYING_ATTRIBUTE);
-            case 2 -> data.getHandle().getDefinition().setAttributeType(headers[escolha], AttributeType.SENSITIVE_ATTRIBUTE);
-            case 3 -> data.getHandle().getDefinition().setAttributeType(headers[escolha], AttributeType.INSENSITIVE_ATTRIBUTE);
-            case 4 -> data.getHandle().getDefinition().setAttributeType(headers[escolha], AttributeType.QUASI_IDENTIFYING_ATTRIBUTE);
+            case 1 ->
+                    dados.getHandle().getDefinition().setAttributeType(dados.getHandle().getAttributeName(escolha), AttributeType.IDENTIFYING_ATTRIBUTE);
+            case 2 ->
+                    dados.getHandle().getDefinition().setAttributeType(dados.getHandle().getAttributeName(escolha), AttributeType.SENSITIVE_ATTRIBUTE);
+            case 3 ->
+                    dados.getHandle().getDefinition().setAttributeType(dados.getHandle().getAttributeName(escolha), AttributeType.INSENSITIVE_ATTRIBUTE);
+            case 4 ->
+                    dados.getHandle().getDefinition().setAttributeType(dados.getHandle().getAttributeName(escolha), AttributeType.QUASI_IDENTIFYING_ATTRIBUTE);
         }
+        // Metódo de transformação
+        System.out.println("Qual o método de transformação que pretende aplicar?\n");
+        System.out.println("1- Generalização");
+        System.out.println("2- Microagregação");
+        System.out.println("3- Clustering e microagregação");
+        int metodo = Ler.umInt();
+        while (metodo < 1 || metodo > 3) {
+            System.out.println("Opção inválida. Tente novamente.");
+            metodo = Ler.umInt();
+        }
+        // Ainda não sei o que meter aqui...
 
-        // Criar Hierarquia
-        System.out.println("Deseja criar uma hierarquia para esta coluna?\n");
+        // Voltar ao menu principal
+        menuPrincipal(dados);
+    }
+
+    public static void menuCriarHierarquias(Data dados, int escolha) {
+        System.out.println("Deseja criar uma hierarquia para esta variável?\n");
         System.out.println("1- Sim");
         System.out.println("2- Não");
         int hierarquia = Ler.umInt();
@@ -100,10 +132,45 @@ public class Main {
             System.out.println("Opção inválida. Tente novamente.");
             hierarquia = Ler.umInt();
         }
+        if (hierarquia == 1) {
+            System.out.println("Qual o tipo de hierarquia que pretende criar?\n");
+            System.out.println("1- Hierarquia de intervalos (para variáveis numéricas)");
+            System.out.println("2- Hierarquia de ordenação (para variáveis com escala ordinal)");
+            System.out.println("3- Hierarquia de marcaramento (para strings alfanuméricas)");
+            int tipoHierarquia = Ler.umInt();
+            while (tipoHierarquia < 1 || tipoHierarquia > 3) {
+                System.out.println("Opção inválida. Tente novamente.");
+                tipoHierarquia = Ler.umInt();
+            }
+            switch (tipoHierarquia) {
+                case 1 -> menuCriarHierarquiasIntervalos(dados, escolha);
+                case 2 -> menuCriarHierarquiasOrdenacao(dados, escolha);
+                case 3 -> menuCriarHierarquiasMarcaramento(dados, escolha);
+            }
+        }
+    }
 
-        // Especificar tipo de hierarquia
-        System.out.println("Qual o tipo de hierarquia que pretende criar?\n");
+    private static void menuExportarDados(Data data) {
+        DataHandle handle = data.getHandle();
+        System.out.println("Qual o nome do ficheiro que pretende exportar?");
+        String nomeFicheiro = Ler.umaString();
+        try {
+            handle.save(nomeFicheiro, ';');
+        } catch (IOException e) {
+            System.out.println("Erro ao exportar os dados.");
+        }
+    }
 
+    private static void menuAnonimizarDados() {
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Qual o ficheiro que pretende utilizar?");
+        String ficheiro = Ler.umaString();
+        Data data = Data.create(ficheiro, Charset.defaultCharset(), ';');
+
+        menuPrincipal(data);
 
     }
 }
